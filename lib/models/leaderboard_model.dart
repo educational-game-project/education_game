cb3c99b87d126dbb91ac85a5462af434c53b9b5e
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2024-02-06 22:12:10 +0700

    update leaderboard model

--- lib/models/leaderboard_model.dart
+++ lib/models/leaderboard_model.dart
@@ -4,15 +4,18 @@ import 'package:education_game/models/user/user_model.dart';
 class LeaderboardModel extends Equatable {
   final UserModel? user;
   final num? value;
+  final bool? isCurrentUser;
 
-  const LeaderboardModel({this.user, this.value});
+  const LeaderboardModel({this.user, this.value, this.isCurrentUser});
 
   factory LeaderboardModel.fromJson(Map<String, dynamic> json) {
     return LeaderboardModel(
-        user: json['user'] == null ? null : UserModel.fromJson(json['user']),
-        value: json['value']);
+      user: json['user'] == null ? null : UserModel.fromJson(json['user']),
+      value: json['value'],
+      isCurrentUser: json['isCurrentUser'],
+    );
   }
 
   @override
-  List<Object?> get props => [user, value];
+  List<Object?> get props => [user, value, isCurrentUser];
 }
