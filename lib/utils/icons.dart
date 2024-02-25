eebf86a746a2df1faa818e859b935f554f7f2c56
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2024-02-25 22:05:08 +0700

    upload all icons and images

--- lib/utils/icons.dart
+++ lib/utils/icons.dart
@@ -3,7 +3,10 @@ class AppIcons {
 
   static const icon =
       'https://res.cloudinary.com/sningratt/image/upload/v1708869662/assets/icons.png';
-  static const back = 'assets/icons/back.png';
-  static const home = 'assets/icons/home.png';
-  static const logo = 'assets/icons/logo.svg';
+  static const back =
+      'https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/back.png';
+  static const home =
+      'https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/home.png';
+  static const logo =
+      'https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/logo.svg';
 }
--- lib/utils/images.dart
+++ lib/utils/images.dart
@@ -2,52 +2,97 @@ class AppImages {
   AppImages._();
 
   // home page
-  static const tombolKeluar = 'assets/icons/tombol_keluar.png';
-  static const kiriAtas = 'assets/icons/kiri_atas.png';
-  static const kananAtas = 'assets/icons/kanan_atas.png';
-  static const center = 'assets/icons/center.png';
-  static const kiriBawah = "assets/icons/kiri_bawah.png";
-  static const kananBawah = "assets/icons/kanan_bawah.png";
-  static const tombolKembali = "assets/icons/back_arrow.png";
-  static const logo = "assets/icons/logo.png";
-  static const think = "assets/icons/think.png";
-  static const batang = "assets/icons/batang.png";
-  static const noGreen = "assets/icons/noGreen.png";
-  static const yesRed = "assets/icons/yesRed.png";
-  static const lineWood = 'assets/icons/lineWood.png';
-  static const lineDotRight = 'assets/icons/lineDotRight.png';
-  static const lineDotTopRight = 'assets/icons/lineDotTopRight.png';
-  static const lineDotBottomLeft = 'assets/icons/lineDotBottomLeft.png';
-  static const iconStart = 'assets/icons/iconStart.png';
-  static const iconExit = 'assets/icons/iconExit.png';
-  static const greenBoard = 'assets/icons/greenBoard.png';
-  static const iconHome = 'assets/icons/iconHome.png';
-  static const iconContinue = 'assets/icons/iconContinue.png';
-  static const home = "assets/icons/home.png";
-  static const sad = "assets/icons/sad.png";
-  static const retry = "assets/icons/retry.png";
-  static const backArrow = "assets/icons/back_arrow.png";
-  static const vectorBottomLeft = "assets/icons/vector_bottom_left.png";
-  static const happyGirl = "assets/icons/happy_girl.png";
-  static const happyBoy = "assets/icons/happy_boy.png";
-  static const lock = "assets/icons/lock.png";
-  static const rateStar = "assets/icons/rateStar.png";
-  static const elephant = "assets/icons/elephant.png";
+  static const tombolKeluar =
+      'https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/tombol_keluar.png';
+  static const kiriAtas =
+      'https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/kiri_atas.png';
+  static const kananAtas =
+      'https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/kanan_atas.png';
+  static const center =
+      'https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/center.png';
+  static const kiriBawah =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/kiri_bawah.png";
+  static const kananBawah =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/kanan_bawah.png";
+  static const tombolKembali =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/back_arrow.png";
+  static const logo =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/logo.png";
+  static const think =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/think.png";
+  static const batang =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/batang.png";
+  static const noGreen =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/noGreen.png";
+  static const yesRed =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/yesRed.png";
+  static const lineWood =
+      'https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/lineWood.png';
+  static const lineDotRight =
+      'https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/lineDotRight.png';
+  static const lineDotTopRight =
+      'https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/lineDotTopRight.png';
+  static const lineDotBottomLeft =
+      'https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/lineDotBottomLeft.png';
+  static const iconStart =
+      'https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/iconStart.png';
+  static const iconExit =
+      'https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/iconExit.png';
+  static const greenBoard =
+      'https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/greenBoard.png';
+  static const iconHome =
+      'https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/iconHome.png';
+  static const iconContinue =
+      'https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/iconContinue.png';
+  static const home =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/home.png";
+  static const sad =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/sad.png";
+  static const retry =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/retry.png";
+  static const backArrow =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/back_arrow.png";
+  static const vectorBottomLeft =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/vector_bottom_left.png";
+  static const happyGirl =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/happy_girl.png";
+  static const happyBoy =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/happy_boy.png";
+  static const lock =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/lock.png";
+  static const rateStar =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/rateStar.png";
+  static const elephant =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/elephant.png";
 
-  static const nanas = "assets/icons/nanas.png";
-  static const mangga = "assets/icons/mangga.png";
-  static const alpukat = "assets/icons/alpukat.png";
-  static const lemon = "assets/icons/lemon.png";
-  static const jeruk = "assets/icons/jeruk.png";
-  static const semangka = "assets/icons/semangka.png";
-  static const apel = "assets/icons/apel.png";
-  static const playAudio = "assets/icons/playAudio.png";
-  static const ayam = "assets/icons/Ayam.png";
-  static const gajah = "assets/icons/Gajah.png";
-  static const singa = "assets/icons/Singa.png";
-  static const kucing = "assets/icons/Kucing.png";
-  static const anjing = "assets/icons/Anjing.png";
-  static const kelinci = "assets/icons/Kelinci.png";
+  static const nanas =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/nanas.png";
+  static const mangga =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/mangga.png";
+  static const alpukat =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/alpukat.png";
+  static const lemon =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/lemon.png";
+  static const jeruk =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/jeruk.png";
+  static const semangka =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/semangka.png";
+  static const apel =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/apel.png";
+  static const playAudio =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/playAudio.png";
+  static const ayam =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/Ayam.png";
+  static const gajah =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/Gajah.png";
+  static const singa =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/Singa.png";
+  static const kucing =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/Kucing.png";
+  static const anjing =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/Anjing.png";
+  static const kelinci =
+      "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/Kelinci.png";
   static const kuda =
       "https://res.cloudinary.com/sningratt/image/upload/v1708869094/assets/kuda.png";
   static const zebra =
